93084bca8b0696877898e4b63eb312d757cca8b6
Raphael Neves <raphaeloneves@outlook.com>
2020-11-05 17:48:44 +0000

    feat: cars filtering by color and maker

--- app/controllers/concerns/cars_concern.rb
+++ app/controllers/concerns/cars_concern.rb
@@ -5,7 +5,12 @@ module CarsConcern
 
   def cars
     @cars ||= Car.available.
+      filtering_by(filtering_params).
       includes({ model: :maker }, :subscription_price).
       limiting_by(params[:limit])
+  end
+
+  def filtering_params
+    @filtering_params ||= params.permit(:color, :maker)
   end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Car < ApplicationRecord
+  extend Filterable
+
   belongs_to :model, class_name: 'Model'
   has_one :subscription_price, class_name: 'SubscriptionPrice', dependent: :destroy
 
@@ -10,5 +12,11 @@ class Car < ApplicationRecord
 
   scope :available, -> { where('available_at <= ?', 3.months.after.to_date) }
   scope :limiting_by, ->(limit) { limit(limit) }
+
+  # filtering scopes
+  scope :by_maker, lambda { |maker_name|
+    joins(model: :maker).where('makers.name like ?', "%#{maker_name}%")
+  }
+  scope :by_color, ->(color) { where('color like ?', "%#{color}%") }
 
 end
