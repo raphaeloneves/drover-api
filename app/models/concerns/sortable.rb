b57a5391475b2a91adf17042b5e103bc7f6955d4
Raphael Neves <raphaeloneves@outlook.com>
2020-11-09 09:09:05 +0000

    refactor: sortable module

--- app/models/concerns/sortable.rb
+++ app/models/concerns/sortable.rb
@@ -7,15 +7,15 @@ module Sortable
   DEFAULT_SORTING_METHOD = 'sorting_default'
 
   def sorting_by(sort_params)
-    return public_send(DEFAULT_SORTING_METHOD) if sort_params[:sort].blank? &&
-                                                  respond_to?(DEFAULT_SORTING_METHOD)
+    return all.public_send(DEFAULT_SORTING_METHOD) if sort_params[:sort].nil? && respond_to?(DEFAULT_SORTING_METHOD)
 
     method = "sorting_by_#{sort_params[:sort]}"
-    public_send(method, sorting_direction(sort_params[:sort_dir])) if respond_to?(method)
+    return all unless respond_to? method
+
+    all.public_send(method, sorting_direction(sort_params[:sort_dir]))
   end
 
   def sorting_direction(param_direction)
     ALLOWED_DIRECTIONS.include?(param_direction&.downcase) ? param_direction : :asc
   end
 end
-
